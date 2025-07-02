3a65265e21f8f52b6396aef0aa102cf1ef9b5ba8
fgorsky <fedor@support-pc.org>
2025-07-02 12:41:47 +0300

    probes

--- pkg/app.go
+++ pkg/app.go
@@ -4,22 +4,90 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
 	"github.com/uptrace/bun/extra/bundebug"
 	"github.com/uptrace/bun/migrate"
 	"github.com/xid/sdk/example/migrations"
+	"net/http"
 	"os"
+	"sync/atomic"
 )
 
+var isReady = &atomic.Value{}
+
+func init() {
+
+	isReady.Store(false)
+}
+
 type Application struct {
-	Db         *bun.DB
-	Migrations *migrate.Migrations
+	Db     *bun.DB
+	Router *gin.Engine
 }
 
 func NewApplication() *Application {
+	return &Application{
+		Db:     initDb(),
+		Router: initRouter(),
+	}
+}
 
+func (a Application) Run() {
+	fmt.Println("Application is running")
+
+	a.AppendReadyProbe()
+	a.AppendHealthProbe()
+
+	done := make(chan bool)
+	go a.Router.Run(os.Getenv("HTTP_ADDR"))
+	isReady.Store(true)
+	<-done
+}
+
+func (a Application) AppendGetEndpoint(route string, handler gin.HandlerFunc) {
+	a.Router.GET(route, handler)
+}
+
+func (a Application) AppendReadyProbe() {
+	a.AppendGetEndpoint("/ready", gin.WrapF(ReadyProbe(isReady)))
+}
+
+func ReadyProbe(isReady *atomic.Value) http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		if isReady == nil || !isReady.Load().(bool) {
+			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func (a Application) AppendHealthProbe() {
+	a.AppendGetEndpoint("/health", HealthWith(a.Db))
+}
+
+func HealthWith(db *bun.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, err := db.Exec("SELECT 1 = 1")
+
+		if err != nil {
+			c.AbortWithStatus(http.StatusServiceUnavailable)
+		}
+
+		c.Writer.WriteHeader(http.StatusOK)
+	}
+}
+
+func initRouter() *gin.Engine {
+	r := gin.Default()
+
+	return r
+}
+
+func initDb() *bun.DB {
 	dsn := fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
 		os.Getenv("DB_USER"),
@@ -53,9 +121,5 @@ func NewApplication() *Application {
 		fmt.Printf("there are no new migrations to run (database is up to date)\n")
 	}
 
-	return &Application{Db: db}
-}
-
-func (a Application) Run() {
-	fmt.Println("Application is running")
+	return db
 }
