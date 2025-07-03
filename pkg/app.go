1d0636c90ab1f7d0fe60fde4053babd0e4366a9e
fgorsky <fedor@support-pc.org>
2025-07-03 08:11:30 +0300

    move db to global

--- pkg/app.go
+++ pkg/app.go
@@ -1,16 +1,9 @@
 package pkg
 
 import (
-	"context"
-	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/driver/pgdriver"
-	"github.com/uptrace/bun/extra/bundebug"
-	"github.com/uptrace/bun/migrate"
-	"github.com/xid/sdk/example/migrations"
 	"net/http"
 	"os"
 	"sync/atomic"
@@ -19,7 +12,6 @@ import (
 var isReady = &atomic.Value{}
 
 func init() {
-
 	isReady.Store(false)
 }
 
@@ -30,7 +22,7 @@ type Application struct {
 
 func NewApplication() *Application {
 	return &Application{
-		Db:     initDb(),
+		Db:     Db,
 		Router: initRouter(),
 	}
 }
@@ -85,41 +77,4 @@ func initRouter() *gin.Engine {
 	r := gin.Default()
 
 	return r
-}
-
-func initDb() *bun.DB {
-	dsn := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-	)
-
-	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
-	db := bun.NewDB(sqldb, pgdialect.New())
-	db.AddQueryHook(bundebug.NewQueryHook(
-		bundebug.WithEnabled(true),
-		bundebug.FromEnv(),
-	))
-
-	ctx := context.Background()
-
-	m := migrate.NewMigrator(db, migrations.Migrations)
-	err := m.Init(ctx)
-	if err != nil {
-		panic(err)
-	}
-	group, err := m.Migrate(ctx)
-	//
-	if err != nil {
-		panic(err)
-	}
-
-	if group.IsZero() {
-		fmt.Printf("there are no new migrations to run (database is up to date)\n")
-	}
-
-	return db
 }
