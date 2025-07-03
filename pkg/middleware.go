b34f8d865d77d193fd56042d0952cfbc8da7763a
fgorsky <fedor@support-pc.org>
2025-07-03 10:31:31 +0300

    extract middleware

---
+++ pkg/middleware.go
@@ -0,0 +1,26 @@
+package pkg
+
+import "github.com/gin-gonic/gin"
+
+func CorsMiddleware() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func JsonMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Next()
+	}
+}
