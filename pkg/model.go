36b3e386480becf9440f11e71bfdf8585788b9dc
fgorsky <fedor@support-pc.org>
2025-07-08 18:21:56 +0300

    model / AfterLoad interface

--- pkg/model.go
+++ pkg/model.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
-	"reflect"
 )
 
 type ModelAfterLoad interface {
@@ -15,22 +14,17 @@ func LoadModel[T interface{}](c *gin.Context, model T, errorMessages map[string]
 	if err := c.ShouldBindJSON(&model); err != nil {
 		var ve validator.ValidationErrors
 		if errors.As(err, &ve) {
-
 			out := make(map[string][]string, len(ve))
-
 			for _, field := range ve {
 				out[field.Field()] = append(out[field.Field()], getErrorMsg(field, errorMessages))
 			}
-
 			return model, out
 		}
 	}
 
-	value := reflect.ValueOf(model)
-	modelType := reflect.Indirect(value).Type()
-	modelValue := reflect.New(modelType)
-	if afterLoadModel, ok := modelValue.Interface().(ModelAfterLoad); ok {
+	if afterLoadModel, ok := interface{}(model).(ModelAfterLoad); ok {
 		afterLoadModel.AfterLoad()
+		return afterLoadModel.(T), nil
 	}
 
 	return model, nil
