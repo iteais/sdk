3d778a5a29405dd9bc369340fe10c0343b0cddc9
fgorsky <fedor@support-pc.org>
2025-07-10 17:52:53 +0300

    tune cache

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -1,7 +1,9 @@
 package cache
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"github.com/eko/gocache/lib/v4/cache"
 	"github.com/eko/gocache/store/go_cache/v4"
 	gocache "github.com/patrickmn/go-cache"
@@ -12,16 +14,33 @@ func GetOrSet[T any](key string, f func() T) T {
 	gocacheClient := gocache.New(5*time.Minute, 10*time.Minute)
 	gocacheStore := go_cache.NewGoCache(gocacheClient)
 
-	cacheManager := cache.New[T](gocacheStore)
+	cacheManager := cache.New[[]byte](gocacheStore)
 
 	cachedValue, err := cacheManager.Get(context.Background(), key)
 	if err == nil {
-		return cachedValue
+
+		var decoded T
+
+		buffer := bytes.NewBuffer(cachedValue)
+		dec := gob.NewDecoder(buffer)
+		err = dec.Decode(&decoded)
+		if err != nil {
+			panic(err)
+		}
+
+		return decoded
 	}
 
 	value := f()
 
-	err = cacheManager.Set(context.Background(), key, value)
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err = enc.Encode(value)
+	if err != nil {
+		panic(err)
+	}
+
+	err = cacheManager.Set(context.Background(), key, buf.Bytes())
 	if err != nil {
 		panic(err)
 	}
