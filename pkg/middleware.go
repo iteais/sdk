18419be60ac7ad05921578b7c3c610c6bc792eef
fgorsky <fedor@support-pc.org>
2025-07-15 12:23:26 +0300

    hmac / check CIDR

--- pkg/middleware.go
+++ pkg/middleware.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"io"
+	"iter"
+	"net"
 	"net/http"
 	"regexp"
 	"strconv"
@@ -60,9 +62,47 @@ type hmacResponse struct {
 	Error string
 }
 
+func localIps() iter.Seq[net.IP] {
+	return func(yield func(net.IP) bool) {
+		addrs, _ := net.InterfaceAddrs()
+		for _, address := range addrs {
+			// check if the address is a loopback or multicast address
+			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+				if ipnet.IP.IsLoopback() {
+					continue
+				}
+
+				yield(ipnet.IP)
+			}
+		}
+	}
+}
+
+func checkSubnet(c *gin.Context, subnet *net.IPNet) bool {
+	clientIP := c.ClientIP()
+
+	// Проверка IP адреса клиента
+	ip := net.ParseIP(clientIP)
+	if ip == nil {
+		return false
+	}
+
+	if subnet.Contains(ip) {
+		return true
+	}
+	return false
+}
+
 // HmacMiddleware Проверка подписи запроса
 func HmacMiddleware(checkHost string, whiteList ...string) gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		for localIp := range localIps() {
+			if checkSubnet(c, &net.IPNet{IP: localIp, Mask: net.CIDRMask(32, 32)}) {
+				c.Next()
+				return
+			}
+		}
 
 		wl := append([]string{MetricsEndpoint, HealthEndpoint, ReadyEndpoint}, whiteList...)
 
